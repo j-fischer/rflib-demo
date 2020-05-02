7c9deff2faf3d704aa2d88a95773529b652536b0
Johannes <fischer.jh@gmail.com>
2020-05-01 18:38:25 -0600

    Added init script for windows including rflib deployment.

---
+++ scripts/orgInit.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+sfdx force:org:create -a rflib_demo -s -f config/project-scratch-def.json -d 30
+
+cd ..\rflib
+
+sfdx force:source:push -u rflib_demo
+sfdx force:user:permset:assign -u rflib_demo -n rflib_Ops_Center_Access
+
+cd ..\rflib_demo
+
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+
+sfdx force:org:open -p /lightning/page/home
+echo "Org is set up"
