818acf0381b3e6187b49635d9b03f255a82f368d
Johannes Fischer <fischer.jh@gmail.com>
2022-11-13 15:58:55 -0700

    Added a of retryable actions using the SampleDataImporter

--- force-app/main/default/aura/SampleDataImporter/SampleDataImporterController.js
+++ force-app/main/default/aura/SampleDataImporter/SampleDataImporterController.js
@@ -23,5 +23,30 @@
             }
         });
         $A.enqueueAction(action);
+    },
+    importDataAsync : function(component, event, helper) {
+        var logger = component.find('logger');
+
+        var action = component.get("c.importSampleDataAsync");
+        component.set("v.showSpinner", true);
+        action.setCallback(this, function (response) {
+            component.set("v.showSpinner", false);
+            var state = response.getState();
+            if (state === "SUCCESS") { 
+                component.find('notifLib').showToast({
+                    "variant": "success",
+                    "header": "Success",
+                    "message": "Scheduled Action to import sample data.",
+                });
+            } else {
+                logger.info(response.getError());
+                component.find('notifLib').showToast({
+                    "variant": "error",
+                    "header": "Error",
+                    "message": "Scheduling action to import Sample data failed.",
+                });
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
