eb6f3966d820e495ab33a90a2d689cb7f313518e
Johannes Fischer <fischer.jh@gmail.com>
2025-01-01 13:01:55 -0700

    Updated instrumentation commands to only take in the force-app folder as source

--- scripts/runRflibPlugin.bat
+++ scripts/runRflibPlugin.bat
@@ -13,11 +13,11 @@ call git reset --hard
 
 echo "Running Apex instrumentation"
 if %PRETTIER%==1 (
-    call sf rflib logging apex instrument --sourcepath force-app/main/default/classes --prettier
-    call sf rflib logging aura instrument --sourcepath force-app/main/default/aura --prettier
-    call sf rflib logging lwc instrument --sourcepath force-app/main/default/lwc --prettier
+    call sf rflib logging apex instrument --sourcepath force-app --prettier
+    call sf rflib logging aura instrument --sourcepath force-app --prettier
+    call sf rflib logging lwc instrument --sourcepath force-app --prettier
 ) else (
-    call sf rflib logging apex instrument --sourcepath force-app/main/default/classes
-    call sf rflib logging aura instrument --sourcepath force-app/main/default/aura
-    call sf rflib logging lwc instrument --sourcepath force-app/main/default/lwc
+    call sf rflib logging apex instrument --sourcepath force-app
+    call sf rflib logging aura instrument --sourcepath force-app
+    call sf rflib logging lwc instrument --sourcepath force-app
 )
