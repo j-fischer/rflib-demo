824dcb8228fee75f378d3c6f2f681ca98f30c9a0
Johannes Fischer <fischer.jh@gmail.com>
2025-01-01 10:45:17 -0700

    Added aura instrumentation to plugin test script.

--- scripts/runRflibPlugin.bat
+++ scripts/runRflibPlugin.bat
@@ -13,8 +13,10 @@ call git reset --hard
 echo "Running Apex instrumentation"
 if %PRETTIER%==1 (
     call sf rflib logging apex instrument --sourcepath force-app/main/default/classes --prettier
+    call sf rflib logging aura instrument --sourcepath force-app/main/default/aura --prettier
     call sf rflib logging lwc instrument --sourcepath force-app/main/default/lwc --prettier
 ) else (
     call sf rflib logging apex instrument --sourcepath force-app/main/default/classes
+    call sf rflib logging aura instrument --sourcepath force-app/main/default/aura
     call sf rflib logging lwc instrument --sourcepath force-app/main/default/lwc
 )
