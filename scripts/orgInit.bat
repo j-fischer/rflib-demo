826e655d8262c35bfaa51d2666d92a1c225af4a9
Johannes Fischer <fischer.jh@gmail.com>
2025-04-12 18:13:11 -0600

    Improved installation process including Pharos

--- scripts/orgInit.bat
+++ scripts/orgInit.bat
@@ -15,12 +15,17 @@ call sf project deploy start --ignore-conflicts
 call sf org assign permset -n dreamhouse
 
 call sf apex run -f apex\resetCustomSettings.apex
+call sf apex run -f apex\pharosPostInstall.apex
 
 call sf org open -p /lightning/page/home
 
-echo "Installing Big Object Utility"
+echo "Installing Pharos"
 call sf package install --package 04t5a000001g4x9AAA -o rflib_demo -w 10
+
+echo "Installing Big Object Utility"
 call sf package install --package 04t7F000003irldQAA -o rflib_demo -w 10
+
+echo "Installing Big Steaming Monitor"
 call sf package install --package 04t1t000003Po3QAAS -o rflib_demo -w 10 && sf org assign permset --name Streaming_Monitor -o rflib_demo
 
 call sf project reset tracking -p
