24af7ad311a42618b154f419b4fd7aaffec35c07
Johannes Fischer <fischer.jh@gmail.com>
2025-12-27 17:31:20 -0700

    Removed Streaming Monitor from init script.

--- scripts/orgInit.bat
+++ scripts/orgInit.bat
@@ -25,9 +25,6 @@ call sf package install --package 04t5a000001g4x9AAA -o rflib_demo -w 10
 echo "Installing Big Object Utility"
 call sf package install --package 04t7F000003irldQAA -o rflib_demo -w 10
 
-echo "Installing Big Steaming Monitor"
-call sf package install --package 04t1t000003Po3QAAS -o rflib_demo -w 10 && sf org assign permset --name Streaming_Monitor -o rflib_demo
-
 call sf project reset tracking -p
 
 echo "Org is set up"
